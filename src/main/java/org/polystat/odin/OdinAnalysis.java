f9717a35abcbf5b868a383bd54dddb051f9dc8ca
Tymur Lysenko <tymur.lysenko@gmail.com>
2021-10-11 11:03:59 +0300

    Resolve object hierarchy in odin analysis

--- src/main/java/org/polystat/odin/OdinAnalysis.java
+++ src/main/java/org/polystat/odin/OdinAnalysis.java
@@ -24,7 +24,9 @@
 package org.polystat.odin;
 
 import com.jcabi.xml.XML;
+import java.util.List;
 import java.util.stream.Collectors;
+import org.cactoos.list.ListOf;
 import org.polystat.Analysis;
 import org.polystat.Xmir;
 import org.polystat.odin.interop.java.EOOdinAnalyzer;
@@ -60,10 +62,46 @@ public final class OdinAnalysis implements Analysis {
     @Override
     public Iterable<String> errors(final String locator) throws Exception {
         final XML xml = this.xmir.repr(locator);
-        final String str = xml.toString();
-        // TODO: recursively retrieve the xml
+        final String str = this.getObjectsHierarchy(xml);
         return this.analyzer.analyze(str).stream()
             .map(OdinAnalysisErrorInterop::message)
             .collect(Collectors.toList());
+    }
+
+    /**
+     * Resolves object hierarchy for the give object represented in XMIR and
+     * returns a well-formed XML.
+     * @param xml XMIR of object to get hierarchy for
+     * @return Well-formed XML containing objects that form a hierarchy in XMIR
+     * @throws Exception on errors
+     */
+    private String getObjectsHierarchy(final XML xml) throws Exception {
+        return String.format(
+            "%s%n%s%n%s",
+            "<objects>",
+            this.resolveObjectHierarchy(xml),
+            "</objects>"
+        );
+    }
+
+    /**
+     * Recursively resolves the decoratees for the given decorator object
+     * represented as XMIR.
+     * @param xml XMIR that represents an object
+     * @return Concatenated XML string containing the whole object hierarchy
+     * @throws Exception on errors
+     */
+    private String resolveObjectHierarchy(final XML xml) throws Exception {
+        String result = xml.toString();
+        for (final String decoratee : xml.xpath("o[@name='@']/@base")) {
+            final List<String> split = new ListOf<>(decoratee.split("\\."));
+            final String name = split.get(split.size() - 1);
+            result = String.format(
+                "%s%s",
+                this.xmir.repr(String.format("\\Phi.%s", name)),
+                result
+            );
+        }
+        return result;
     }
 }
